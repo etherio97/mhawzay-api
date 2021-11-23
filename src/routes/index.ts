51b701fcbd233dd0cfdf18954abeca3df1feea4e
Shin Maung Maung <shinmaungmaung@mytel.com.mm>
2021-11-23 12:46:31 +0630

    a

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
+import { verifyAuthToken } from "@mhawzay/middlewares";
 import { Router } from "express";
 import { router as manage } from "./manage";
 
 export const router = Router();
 
-router.use("/manage", manage);
+router.use("/manage", verifyAuthToken, manage);
