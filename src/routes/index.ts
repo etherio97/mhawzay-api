11705a7c8da061e885c3948a4a488bffd5d588d0
Shin Maung Maung <shinmaungmaung@mytel.com.mm>
2021-11-23 12:47:27 +0630

    a

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,3 +5,7 @@ import { router as manage } from "./manage";
 export const router = Router();
 
 router.use("/manage", verifyAuthToken, manage);
+
+router.all("*", (req, res) =>
+  res.status(404).json({ status: 404, error: "Not Found" })
+);
